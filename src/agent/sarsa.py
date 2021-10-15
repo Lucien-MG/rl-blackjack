f0903db4b4be14ba5994b0808d7c9c1e7ed00825
Lucien Martin-Gaffé <lucien.martingaffe@gmail.com>
2021-10-15 16:29:07 +0200

    Fix sarsa aglorithm

--- src/agent/sarsa.py
+++ src/agent/sarsa.py
@@ -12,6 +12,8 @@ class Agent:
     """
     def __init__(self, nb_actions, epsilon=0.99, alpha=1e-2, gamma=1e-1, min_epsilon=1e-4, epsilon_decay_factor=1e-5):
         self.nb_actions = nb_actions
+        self.action = -1
+        
         self.q_table = defaultdict(lambda: np.zeros(self.nb_actions))
         self.memory = deque()
 
@@ -22,6 +24,15 @@ class Agent:
         self.alpha = alpha
         self.gamma = gamma
 
+    def _set_parameters(self, configuration):
+        self.__dict__ = {k:v.value for (k,v) in configuration["parameters"].items()}
+
+    def policy(self, state):
+        if np.random.uniform() > self.epsilon:
+            return np.argmax(self.q_table[state])
+
+        return np.random.choice(self.nb_actions)
+
     def select_action(self, state) -> int:
         """ Given the state, select an action.
 
@@ -31,10 +42,14 @@ class Agent:
         Returns:
             action (int): an integer compatible with the task's action space.
         """
-        if np.random.uniform() > self.epsilon:
-            return np.argmax(self.q_table[state])
+        action_to_take = self.action
 
-        return np.random.choice(self.nb_actions)
+        if action_to_take < 0:
+            action_to_take = policy(state)
+
+        self.action = -1
+
+        return action_to_take
 
     def step(self, state, action, reward, next_state, done):
         """ Update the agent's knowledge, using the most recently sampled tuple.
@@ -46,8 +61,9 @@ class Agent:
             next_state (obj): the current state of the environment
             done (bool): whether the episode is complete (True or False)
         """
+        self.action = self.policy(next_state)
 
-        target_reward = reward + self.gamma * np.mean(self.q_table[next_state])
+        target_reward = reward + self.gamma * self.q_table[next_state][self.action]
         error = target_reward - self.q_table[state][action]
 
         self.q_table[state][action] += self.alpha * error
